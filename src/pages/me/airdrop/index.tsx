6983a974a9764a6e168c7d5b17d3ac8290d693c0
Hikaru Chang <i@rua.moe>
2023-11-16 03:37:54 +0900

    update: airdrop

--- src/pages/me/airdrop/index.tsx
+++ src/pages/me/airdrop/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./style.less";
 import { Button, Modal } from "antd";
+import { ReactComponent as RankIcon } from '@/assets/me/rank.svg';
 
 const Pending: React.FC = () => {
   return (
@@ -18,6 +19,59 @@ const Pending: React.FC = () => {
       </div>
       <div className={styles.airdropContent}>
         Points are airdropped every Friday and will have future uses in AIME.
+      </div>
+      <div className={styles.airdropTable}>
+        <div className={styles.airdropTableItem}>
+          <div className={styles.airdropTableItemTitle}>
+            Your Points:
+          </div>
+          <div className={styles.airdropTableItemValue}>
+            2 Points
+          </div>
+        </div>
+        <div className={styles.airdropTableItem}>
+          <div className={styles.airdropTableItemTitle}>
+            Weekly leaderboard:
+          </div>
+          <div className={styles.airdropTableItemValue}>
+            #16,740
+          </div>
+        </div>
+        <div className={styles.airdropTableItem}>
+          <div className={styles.airdropTableItemTitle}>
+            Your rank:
+          </div>
+          <div className={styles.airdropTableItemValue}>
+            <RankIcon
+              className={styles.airdropTableItemValueRankIcon}
+            />
+            BRONZE
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+const Success: React.FC<{
+  points?: number;
+  name?: string;
+}> = ({ points, name }) => {
+  return (
+    <div className={styles.airdropContainer}>
+      <div className={styles.airdropHeader}>
+        <div className={styles.airdropHeaderSuccessIcon}>
+          <img
+            className={styles.airdropHeaderSuccessIconImg}
+            src={require("@/assets/me/airdrop_success.png")}
+          />
+        </div>
+        <div className={styles.airdropHeaderTitle}>
+          Airdrop Rewards
+        </div>
+      </div>
+      <div className={styles.airdropContent}>
+        Congratulations on receiving Elon Musk’s 1 Power reward.
       </div>
     </div>
   )
@@ -37,7 +91,7 @@ const AirDrop: React.FC<{
       onCancel={() => setVisible(false)}
       closeIcon={null}
     >
-      <Pending />
+      <Success />
       <div className={styles.airdropFooter}>
         <Button
           block
